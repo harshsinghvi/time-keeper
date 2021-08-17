c6434f081fb2289426b89d09fa913619daeee490
Harsh <root@harshsinghvi.com>
2021-08-18 03:33:27 +0530

    Update Sample data

--- src/sampleData.js
+++ src/sampleData.js
@@ -1,4 +1,4 @@
-export const sampleText = "Sample Text/Notes";
+export const sampleText = "Sample Text/Notes \nYou can Add new Workspaces from the end of the list and delete existing Workspaces.";
 export const sampleData = [
   { name: "Workspace 0", color: "#2596be", text: sampleText },
   { name: "Workspace 1", color: "#0040ff", text: sampleText },
